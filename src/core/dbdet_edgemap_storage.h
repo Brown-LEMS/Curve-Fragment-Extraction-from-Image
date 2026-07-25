59a841bb692a91d810096a428efd15ffcbac0d33
zachMahan64 <zach.mahan.zm@gmail.com>
2026-07-25 16:29:24 -0400

    clean up formatting in some important files

--- src/core/dbdet_edgemap_storage.h
+++ src/core/dbdet_edgemap_storage.h
@@ -14,52 +14,45 @@
 // \endverbatim
 
 #include "bpro1_storage.h"
-#include "dbdet_edgemap_storage_sptr.h"
 #include "dbdet_edgemap_sptr.h"
+#include "dbdet_edgemap_storage_sptr.h"
 
 //: Storage class for the symbolic edge linker object
 class dbdet_edgemap_storage : public bpro1_storage {
-public:
+  public:
+    //: Constructor
+    dbdet_edgemap_storage() {}
 
-  //: Constructor
-  dbdet_edgemap_storage() {}
+    //: Destructor
+    virtual ~dbdet_edgemap_storage() {}
 
-  //: Destructor
-  virtual ~dbdet_edgemap_storage(){}
+    //: Returns the type string "edge_map"
+    virtual vcl_string type() const { return "edge_map"; }
 
-  //: Returns the type string "edge_map"
-  virtual vcl_string type() const { return "edge_map"; }
+    //: Return IO version number;
+    short version() const;
 
-  //: Return IO version number;
-  short version() const;
+    //: Create a copy of the object on the heap.
+    // The caller is responsible for deletion
+    virtual bpro1_storage* clone() const;
 
-  //: Create a copy of the object on the heap.
-  // The caller is responsible for deletion
-  virtual bpro1_storage* clone() const;
-  
-  //: Return a platform independent string identifying the class
-  virtual vcl_string is_a() const { return "dbdet_edgemap_storage"; }
+    //: Return a platform independent string identifying the class
+    virtual vcl_string is_a() const { return "dbdet_edgemap_storage"; }
 
-  void set_edgemap(dbdet_edgemap_sptr edgemap) { edge_map_= edgemap; }
-  dbdet_edgemap_sptr get_edgemap() { return edge_map_; }
+    void set_edgemap(dbdet_edgemap_sptr edgemap) { edge_map_ = edgemap; }
+    dbdet_edgemap_sptr get_edgemap() { return edge_map_; }
 
-private:
-
-  //: edge map structure
-  dbdet_edgemap_sptr edge_map_;
-
+  private:
+    //: edge map structure
+    dbdet_edgemap_sptr edge_map_;
 };
-
 
 //: Create a smart-pointer to a dbdet_edgemap_storage.
-struct dbdet_edgemap_storage_new : public dbdet_edgemap_storage_sptr
-{
-  typedef dbdet_edgemap_storage_sptr base;
+struct dbdet_edgemap_storage_new : public dbdet_edgemap_storage_sptr {
+    typedef dbdet_edgemap_storage_sptr base;
 
-  //: Constructor - creates a default dbdet_edgemap_storage_sptr.
-  dbdet_edgemap_storage_new() : base(new dbdet_edgemap_storage()) { }
-
+    //: Constructor - creates a default dbdet_edgemap_storage_sptr.
+    dbdet_edgemap_storage_new() : base(new dbdet_edgemap_storage()) {}
 };
 
-
-#endif //dbdet_edgemap_storage_h_
+#endif // dbdet_edgemap_storage_h_
