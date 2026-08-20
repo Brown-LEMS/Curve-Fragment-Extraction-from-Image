3ba46cdd9043c09fa6d556a6268974c282752453
zachmahan <zach.mahan.zm@gmail.com>
2026-08-19 20:27:40 -0400

    update visualizer demo with improved code

--- demo_vis_io.m
+++ demo_vis_io.m
@@ -1,30 +1,50 @@
 addpath (genpath('util/'));
 
-img = imread('example_data/cabinet.png');
-[h,w,~]= size(img);
-
-% load in edges and contours
-% [edges, edgemap, thetamap] = load_edg('example_data/cabinet.edg');
-% [CEM, edges, cfrags_idx] = load_contours('example_data/cabinet.cem');
-
-[edges, edgemap, thetamap] = load_edg('out.edg');
-[CEM, edges, cfrags_idx] = load_contours('out.cem');
+visualize_contours( ...
+    'example_data/cabinet.png', ...
+    'example_data/cabinet.edg', ...
+    'example_data/cabinet.cem' ...
+);
 
 
-% visualize edges
-figure(1);
-imshow(edgemap, 'border', 'tight');
+visualize_contours( ...
+    'example_data/cabinet.png', ...
+    'out.edg', ...
+    'out.cem' ...
+);
 
-pause(0.3);
+function visualize_contours(image_path, edg_path, cem_path)
 
-% visualize contours
-figure(2);
-contour_min_length = 2;
-rand_color = 1;
-imshow(img, 'border', 'tight'); 
-hold on;
-draw_contours(CEM{2}, contour_min_length, rand_color);
+    addpath(genpath('util/'));
 
-%> Optional: output edges and contour files
-% write_cem('cabinet.cem', CEM{2}, h, w)
-% save_edg('cabinet.edg', edges, [w,h])
+    % load image
+    img = imread(image_path);
+    [h, w, ~] = size(img);
+
+    % load edges
+    [edges, edgemap, thetamap] = load_edg(edg_path);
+
+    % load contours
+    [CEM, edges, cfrags_idx] = load_contours(cem_path);
+
+    % visualize edges
+    figure;
+    imshow(edgemap, 'border', 'tight');
+    pause(0.3);
+
+    % visualize contours
+    figure;
+
+    contour_min_length = 2;
+    rand_color = 1;
+
+    imshow(img, 'border', 'tight');
+    hold on;
+
+    draw_contours(CEM{2}, contour_min_length, rand_color);
+
+    % Optional: output edges and contour files
+    % write_cem('cabinet.cem', CEM{2}, h, w);
+    % save_edg('cabinet.edg', edges, [w, h]);
+
+end
